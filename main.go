ba271b3a010389971262e4ec8e75c594a613294e
Matthew Rothenberg <mrothenberg@gmail.com>
2016-08-02 17:22:13 -0400

    feature: write pem certs to filesystem

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"crypto/rsa"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
+	"path"
 	"runtime"
 	"strings"
 )
@@ -12,11 +15,15 @@ import (
 func main() {
 	var threads = flag.Int("t", runtime.NumCPU(), "number of simultaneous hashing `threads`")
 	var continuous = flag.Bool("c", false, "continuously search for multiple matches")
-	var output = flag.String("output", "", "write keys to filesystem `directory` *TODO*")
+	var output = flag.String("output", "", "write keys to filesystem `directory`")
 	var prefix = flag.String("p", "", "search for hashes matching `prefix`")
 
 	flag.Parse()
 	*prefix = strings.ToUpper(*prefix) // normalize prefix to uppercase
+	if *output != "" && !dirExists(*output) {
+		fmt.Println("Output directory does not exist!:", *output)
+		os.Exit(1)
+	}
 
 	results := make(chan *rsa.PrivateKey)
 	for t := 1; t <= *threads; t++ {
@@ -24,12 +31,23 @@ func main() {
 	}
 
 	for key := range results {
-		fmt.Printf("Found matching domain: %s.onion\n", strings.ToLower(OnionNameString(key)))
+		onionName := strings.ToLower(OnionNameString(key))
+		pem := encPrivKey(key)
 
 		if *output == "" {
-			fmt.Println(string(encPrivKey(key)))
+			// output to screen
+			fmt.Printf("Found matching domain: %s.onion\n", onionName)
+			fmt.Printf("%s\n", pem)
 		} else {
-			// TODO: if in Output mode, write PEM to file instead of stdout
+			// output to disk
+			filepath := path.Join(*output, onionName+".pem")
+			err := ioutil.WriteFile(filepath, pem, 0644)
+
+			if err == nil {
+				log.Println("Stored matching domain for", onionName+".onion")
+			} else {
+				log.Fatal(err)
+			}
 		}
 
 		if !*continuous {
@@ -37,4 +55,12 @@ func main() {
 		}
 	}
 
+}
+
+func dirExists(path string) bool {
+	info, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+	return info.Mode().IsDir()
 }
