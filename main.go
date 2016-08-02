be196e4f402d509e80fd1b0106331c11a58a2e37
Matthew Rothenberg <mrothenberg@gmail.com>
2016-08-02 15:27:27 -0400

    make D value readjustment work
    Figured this out by examining the rsa.c for ffp-0.0.8, blast from the
    past of 2003 and apparently the last time code for this technique was
    written in a semi legible way.
    
    The issue was we needed to recalculate dmp1 and dmq1, which I
    mistakenly thought Go would do automatically once we adjusted D, but
    the Precalculate() function short circuits if it detects any existing
    values in the Precomputed struct.

--- main.go
+++ main.go
@@ -17,6 +17,8 @@ var output = flag.String("output", "", "write keys to filesystem `directory` *TO
 var prefix = flag.String("p", "", "search for hashes matching `prefix`")
 
 func KeyHasher(results chan *rsa.PrivateKey) {
+	var bigOne = big.NewInt(1)
+
 	for {
 		// Generate a new RSA keypair.
 		//
@@ -30,27 +32,41 @@ func KeyHasher(results chan *rsa.PrivateKey) {
 			key, err = NewKey()
 		}
 
+		// Preset some variables outside of the inner loop, in order to avoid
+		// unncessary big.Int allocations.
+		p, q := key.Primes[0], key.Primes[1]
+		p1 := new(big.Int).Sub(p, bigOne) // p - 1
+		q1 := new(big.Int).Sub(q, bigOne) // q - 1
+		r0 := new(big.Int).Mul(p1, q1)    // (p-1)(q-1)
+
 		// Enumerate possibilities for the public exponent of the public key, and
 		// compute the onion hash for each and compare for a matching partial
 		// collision.
+		//
+		// This method is known as "sloppy" key enumeration, we could be more
+		// accurate by ***TODO write explanation
 		for e := E_MIN; e <= E_MAX; e += 2 {
-			// key.PublicKey.E = e
 			key.E = e
 			name := OnionNameString(key)
 			if strings.HasPrefix(name, *prefix) {
-				// TODO: crazy wacky key munging?!
-				// based on https://github.com/ChrisCalderon/PyShallot/blob/2922e00a66ea485ad723be2d1149f62ce6a083cb/shallot.py#L106-L129
-				bE := new(big.Int).SetInt64(int64(e))
-				pq := new(big.Int).Add(key.Primes[0], key.Primes[1])
-				pq1 := new(big.Int).Sub(pq, big.NewInt(1))
-				tot := new(big.Int).Sub(key.N, pq1)
-				key.D = key.D.ModInverse(bE, tot)
-				// NOTE: this tends to modify the keys so they pass valid check here,
-				// but then trying to verify with `openssl rsa -in foo.pem -check` gets:
-				// RSA key error: dmp1 not congruent to d
-				// RSA key error: dmq1 not congruent to d
+				// Some code here (very roughly) based on FFP-0.0.8 rsa.c
 
-				// TODO: verify key validity
+				// We have a match!  Now we recalculate D
+				//rsa->d = BN_mod_inverse(rsa->d, rsa->e, r0, ctx2);
+				bE := new(big.Int).SetInt64(int64(e))
+				key.D.ModInverse(bE, r0)
+
+				// Force recalculate d mod (p-1) [dmp1] and d mod (q-1) [dmq1]
+				//
+				// key.Precompute() could do this... because of !nil guard, would only
+				// work if *certain* has not already called, because Precompute() will
+				// silently return haveing done nothing if it detects dmp1 != nil.
+				key.Precomputed.Dp.Mod(key.D, p1)
+				key.Precomputed.Dq.Mod(key.D, q1)
+
+				/* Verify key validity */
+				// We were doing "sloppy" key generation for speed, so may still be
+				// invalid even after we recalculate values.
 				verificationErr := key.Validate()
 				if verificationErr != nil {
 					log.Printf("match %s with e=%d has verification error: %s\n", name, e, verificationErr)
